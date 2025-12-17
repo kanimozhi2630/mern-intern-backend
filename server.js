404395e6b4736817bee2dd844bc13e4505407df5
kani <kanimozhi301006@gmail.com>
2025-12-18 01:11:06 +0530

    mern backend final

--- server.js
+++ server.js
@@ -11,7 +11,16 @@ const app = express();
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000', 
+    'https://mern-intern-frontend.vercel.app',
+    'https://mern-intern-frontend-git-main-kanimozhis-projects-b47c4c8c.vercel.app',
+    /\.vercel\.app$/
+  ],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
